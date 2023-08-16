e336064de7b82f3ec7374d83fa357b6f1461993f
younssue <108864140+younssue@users.noreply.github.com>
2023-08-16 18:33:30 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+       for(int i =0; i<arr.length; i++){
+            sum += arr[i];
+       }
+        double avg = (double)sum/arr.length;
+        
+       return avg;
+    }
+}
