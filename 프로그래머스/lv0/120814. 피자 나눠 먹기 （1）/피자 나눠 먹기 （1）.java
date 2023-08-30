2dcae05175788d275c9c523def442bdc37be9ef5
younssue <108864140+younssue@users.noreply.github.com>
2023-08-30 23:39:42 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n%7 == 0){
+            return answer= n/7;
+        }else {
+            answer = n/7 + 1;
+        }
+        return answer;
+    }
+}
