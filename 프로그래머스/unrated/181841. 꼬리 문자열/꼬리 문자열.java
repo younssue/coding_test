390688415b4ebbf044de91b398b329fd92a03a91
yeounsueKim <108864140+younssue@users.noreply.github.com>
2023-11-16 11:51:54 +0900

    [level unrated] Title: 꼬리 문자열, Time: 2.16 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String[] str_list, String ex) {
-        String answer = "";
+       // String answer = "";
         
-        for(int i =0; i< str_list.length; i++){
+       // for(int i =0; i< str_list.length; i++){
             // ex 를 포함한 문자열은 제외
-            if(!str_list[i].contains(ex)){
-                answer += str_list[i];
-            }
-        }
-        return answer;
+       //     if(!str_list[i].contains(ex)){
+       //         answer += str_list[i];
+       //     }
+       // }
+        
+        return Arrays.stream(str_list)
+                    .filter(a -> !a.contains(ex))
+                    .collect(Collectors.joining());
     }
 }
