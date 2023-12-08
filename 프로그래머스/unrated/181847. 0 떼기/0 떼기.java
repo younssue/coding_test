1d59a4699a2275d05acc140ecc18a0e2faac1a58
yeounsueKim <108864140+younssue@users.noreply.github.com>
2023-12-08 11:45:19 +0900

    [level unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int num = Integer.parseInt(n_str);
+        answer = Integer.toString(num);
+        
+        return answer;
+    }
+}
