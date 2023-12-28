e083b7cbfb2d149399cb2edcca1b4184bb3329f3
yeounsueKim <108864140+younssue@users.noreply.github.com>
2023-12-28 11:36:23 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution{
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+       //Solution s = new Solution();
+       //입력요소
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id+"??!");
+    }
+}
