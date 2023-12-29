25750f2a4f1f1f96a64a1493ca38dd3a72a750b7
yeounsueKim <108864140+younssue@users.noreply.github.com>
2023-12-29 11:41:03 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Solution{
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+       //Solution s = new Solution();
+       //입력요소
+        Scanner sc = new Scanner(System.in);
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();;
+
+        sc.close();
+
+
+        System.out.println(A+B+C);
+
+
+
+    }
+}
