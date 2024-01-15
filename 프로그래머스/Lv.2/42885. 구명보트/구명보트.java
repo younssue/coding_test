305f27e82c940a440ff9245b98a94cd862a51301
yeounsueKim <108864140+younssue@users.noreply.github.com>
2024-01-15 16:48:41 +0900

    [level Lv.2] Title: 구명보트, Time: 8.98 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int i = 0;
+        int j = people.length - 1;
+        Arrays.sort(people);
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
