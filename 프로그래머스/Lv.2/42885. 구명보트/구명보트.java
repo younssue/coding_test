dcd9b249ef4e8d528dc7a3629ed8c2f437ad5ac5
yeounsueKim <108864140+younssue@users.noreply.github.com>
2024-01-16 21:50:54 +0900

    [level Lv.2] Title: 구명보트, Time: 22.32 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
+++ 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
@@ -1,20 +1,25 @@
 import java.util.Arrays;
-
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int i = 0;
-        int j = people.length - 1;
+        int i=0;
+        int j=people.length-1;
         Arrays.sort(people);
-
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
+        
+        //[50,50,70,80]
+        while(i<=j){
+            if(people[i]+people[j] <= limit){
+                
                 i++;
+                
             }
-            j--;
-            answer++;
+            
+                j--;
+                answer++;
+            
+            
         }
-
+        
         return answer;
     }
 }
