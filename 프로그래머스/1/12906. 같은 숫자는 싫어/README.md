dca47781e5d6caf3a74d29632824481808e593cc
yeounsueKim <108864140+younssue@users.noreply.github.com>
2024-01-22 11:34:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 53.17 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,6 +1,6 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 11:33:39
+2024년 1월 1일 11:34:31
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,24 +2,10 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-       
+        int[] answer = {};
         
-        Stack<Integer> stack = new Stack<Integer>();
-        
-        for(int i=0; i<arr.length; i++){
-            if(i==0){
-                stack.push(arr[i]);
-            }else if(stack.peek() != arr[i]){
-                stack.push(arr[i]);
-            }
-        }
-        
-        int[] answer  = new int[stack.size()];
-        for(int i=stack.size()-1; i>=0; i--){
-            answer[i] = stack.pop();
-        }
-        
-
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
