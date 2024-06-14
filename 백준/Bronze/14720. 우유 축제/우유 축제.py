21f9954d3f132cc819c2286188c4b48fb22185d6
yeounsueKim <108864140+younssue@users.noreply.github.com>
2024-06-14 20:49:31 +0900

    [Bronze II] Title: 우유 축제, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.py
+++ 백준/Bronze/14720. 우유 축제/우유 축제.py
@@ -5,10 +5,11 @@ milk_num = 0
 count = 0
 
 for i in milk_list:
+    if milk_num > 2:
+        milk_num = 0
+        
     if i == milk_num:
         count += 1
         milk_num += 1
-
-    if milk_num > 2:
-        milk_num = 0
+        continue
 print(count)
